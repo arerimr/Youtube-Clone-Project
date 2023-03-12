3b19b2c324fab15b35d312adb6ffd212fc4b013a
arerimr <raydelys.morrobelr@gmail.com>
2023-03-12 17:02:58 -0400

    added search feature to navBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,19 @@
 import "./NavBar.css"
 import { Link } from "react-router-dom"
+import logo from "../Assets/whiteLogo.PNG"
+import Search from "./Search"
+import { useLocation } from "react-router-dom"
 
 function NavBar() {
+
+  let loc = useLocation()
+
   return (
     <div className="Nav-bar">
-      <Link to="/">
-        <h2>YÚ Tub</h2>
-      </Link>
       <ul>
+        <Link to="/">
+          <li className="header"><img src={logo} alt="logo" /> YÚ Tub</li>
+        </Link>
         <Link to="/">
           <li><strong>Home</strong></li>
         </Link>
@@ -15,6 +21,9 @@ function NavBar() {
           <li><strong>About</strong></li>
         </Link>
       </ul>
+      <div className="navSearch" style={loc.pathname === "/" ? { display: "none" } : { display: "block" }}>
+        <Search />
+      </div>
     </div>
   )
 }
