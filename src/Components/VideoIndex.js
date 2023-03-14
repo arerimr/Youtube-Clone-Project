eed0e2f4b92a2ebda139508fdbd9f54abb689947
Wilsantos1975 <Faustosantos@pursuit.org>
2023-03-14 19:21:17 -0400

    merge conflicts

--- src/Components/VideoIndex.js
+++ src/Components/VideoIndex.js
@@ -1,30 +1,16 @@
-
-
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useLocation, Link } from "react-router-dom";
-
-// import VideoShowPage from "./VideoShowPage";
 import ErrorMessage from "../Components/errors/ErrorMessage";
-import { Link } from "react-router-dom";
 
 // const { id } = useParams();
 
-
-function VideoIndex({searchInput}) {
-
+function VideoIndex() {
   const [loadingError, setLoadingError] = useState(false);
 
   const location = useLocation();
   const videoList = location.state.res;
 
   console.log(videoList);
-
-  function filterVideo(search, videos) {
-    return videos.filter((video) => {
-      return video.title.toLowerCase().match(search.toLowerCase());
-    });
-  }
-
   return (
     <div className="video-index">
       <p>display videos</p>
@@ -36,16 +22,15 @@ function VideoIndex({searchInput}) {
             return (
               <ul>
                 <li key={video.id.videoId}>
-                 <Link to={`/videos/${video.id.videoId}`}>
-                 <img
-                    src={video.snippet.thumbnails.medium.url}
-                    alt={video.snippet.title}
-                  />
-                  <h3>{video.snippet.title}</h3>
-                 </Link> 
+                  <Link to={`/videos/${video.id.videoId}`}>
+                    <img
+                      src={video.snippet.thumbnails.medium.url}
+                      alt={video.snippet.title}
+                    />
+                    <h3>{video.snippet.title}</h3>
+                  </Link>
                 </li>
               </ul>
-
             );
           })
         )}
